8e8285ce66aa771fb1c60fdddeee54dc187f7768
DichterDev <dichterdev.null@gmail.com>
2026-07-03 14:08:16 +0200

    chore(repo): fix postgres version 18 compatibility issue

--- infra/postgres/01-init.sql
+++ infra/postgres/01-init.sql
@@ -3,6 +3,6 @@ CREATE DATABASE gout_prod;
 CREATE TABLE users (
     id UUID PRIMARY KEY,
     name TEXT NOT NULL,
-    password TEXT NOT NULL,
-    is_active BOOLEAN NOT NULL DEFAULT true
+    PASSWORD TEXT NOT NULL,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE
 );
